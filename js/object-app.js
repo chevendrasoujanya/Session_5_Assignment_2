1668dcc01bec024c331fd415e1510978812374cb
chevendrasoujanya <kesar989@gmail.com>
2016-12-02 10:07:08 +0530

    Add files via upload

---
+++ js/object-app.js
@@ -0,0 +1,8 @@
+var myObject = {
+	name: 'Soujanya',
+	age: 28,
+	dob: '14-April-2016',
+	placeOfBirth: 'Hyderabad'
+}
+
+console.log("Name: " + myObject.name + "\n" + "Age: " + myObject.age + "\n" + "DOB: " + myObject.dob + "\n" + "Place of Birth: " + myObject.placeOfBirth);
